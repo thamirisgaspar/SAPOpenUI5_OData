f2dffbc901ab670a56a74de33bf9e96e4af9bbba
T. Gaspar Da Silva <t.gaspar.da.silva@avanade.com>
2020-03-24 22:42:41 -0300

    OData Operations

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -76,6 +76,18 @@ sap.ui.define([
 			this._bTechnicalErrors = false; 
 			this._setUIChanges();
 		},
+		onResetDataSource : function () {
+			var oModel = this.getView().getModel(),
+				oOperation = oModel.bindContext("/ResetDataSource(...)");
+
+			oOperation.execute().then(function () {
+					oModel.refresh();
+					MessageToast.show(this._getText("sourceResetSuccessMessage"));
+				}.bind(this), function (oError) {
+					MessageBox.error(oError.message);
+				}
+			);
+		},
 		onSave : function () {
 			var fnSuccess = function () {
 				this._setBusy(false);
